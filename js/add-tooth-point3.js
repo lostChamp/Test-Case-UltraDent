8cefc7c9a0c8ef1ed70f945a9fef3bc6294a8bf2
Kostya <lost.champ2007@gmail.com>
2023-11-29 11:08:06 +0500

    Bugfix: p.3 return array

--- js/add-tooth-point3.js
+++ js/add-tooth-point3.js
@@ -6,16 +6,16 @@ for(let i = 1; i <= 32; i++) {
     if(i === 16 || i === 32) {
         teeth += `
         <div class="tooth">
-            <input type="checkbox" value="${i}" id="tooth-${i}" name="tooth" class="tooth__checkbox">
+            <input type="checkbox" value="${i}" id="tooth-${i}" name="tooth[]" class="tooth__checkbox">
             <label for="tooth-${i}" class="tooth__mark"></label>
         </div>
     `
     }else {
         teeth += `
         <div class="tooth">
-            <input type="checkbox" value="${i}" id="tooth-${i}" name="tooth" class="tooth__checkbox">
+            <input type="checkbox" value="${i}" id="tooth-${i}" name="tooth[]" class="tooth__checkbox">
             <label for="tooth-${i}" class="tooth__mark"></label>
-            <input type="checkbox" value="${i}-${i + 1}" id="tooth-link-${i}" name="tooth-link" class="tooth__checkbox">
+            <input type="checkbox" value="${i}-${i + 1}" id="tooth-link-${i}" name="tooth-link[]" class="tooth__checkbox">
             <label for="tooth-link-${i}" class="tooth-link__mark"></label>
         </div>
     `
