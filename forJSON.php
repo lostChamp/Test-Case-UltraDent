acf63b725e359bdad799d3910510e81e659da311
Kostya <lost.champ2007@gmail.com>
2023-11-29 12:53:26 +0500

    Send POST from get-post.php

---
+++ forJSON.php
@@ -0,0 +1,3 @@
+<?php
+$res = file_get_contents("php://input");
+echo $res;
--- get-post.php
+++ get-post.php
@@ -9,5 +9,35 @@ if(!empty($_POST)) {
             print $key . " = " . $value . "</br>";
         }
     }
-    print(json_encode($_POST, JSON_UNESCAPED_UNICODE));
+
+
+    $url = "http://localhost/forJSON.php";
+
+    $ch = curl_init();
+
+    $payload = json_encode(json_encode($_POST, JSON_UNESCAPED_UNICODE));
+
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $payload,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+    ));
+
+    $response = curl_exec($curl);
+
+    echo $response;
+
+    curl_close($curl);
 }
